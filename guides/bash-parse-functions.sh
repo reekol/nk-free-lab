a63b22d7c01e6c1a927a0a2497013507c9128ec6
reekol <cupuyc@gmail.com>
2020-01-09 20:28:55 +0200

    Initial commit

---
+++ guides/bash-parse-functions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+getPos(){
+    echo $(echo $1 | grep -b -o "$2" | cut -d':' -f1 | head -n $3 | tail -n1)
+}
+
+substr(){
+    local fr=$(getPos "$1" "$2" "$3")
+    local to=$(getPos "$1" "$4" "$5")
+    echo $1 | cut -b $(($fr+1))-$to | sed 's/amp\;//g' | grep -v -e '^[[:space:]]*$'
+}
+
+urlCache(){
+    url=$(substr $1 "http" 1 '"' 2)
+    bnm=$(basename $url)
+    cch="$PWD/cache/$bnm"
+    [ -f  $cch ] || curl -s -o $cch $url
+    echo $cch
+}
