5bbfd04b9618c3bf9f13346a62205a711bd3dec6
N.T.Nitro5 <n.terziev@elysiumstudios.se>
2023-01-18 16:06:12 +0200

    Add openvpn setup

---
+++ infrastructure/config/openvpn_setup.sh
@@ -0,0 +1,71 @@
+source .env.dev
+
+openvpn_setup_vpn () {
+
+  docker container exec ${PREFIX}_setupc /bin/sh -c "
+    apk add bind-tools iptables openssl openvpn openvpn-auth-ldap easy-rsa
+
+    mkdir -p /dev/net
+    mknod /dev/net/tun c 10 200
+
+    /usr/share/easy-rsa/easyrsa --batch init-pki
+    /usr/share/easy-rsa/easyrsa --batch build-ca nopass
+    /usr/share/easy-rsa/easyrsa --batch gen-dh
+    /usr/share/easy-rsa/easyrsa --batch build-server-full  vpn.${DOMAIN}   nopass
+
+    iptables -A INPUT   -i eth0 -m state --state NEW -p udp --dport 1194 -j ACCEPT
+    iptables -A INPUT   -i tun+ -j ACCEPT
+    iptables -A FORWARD -i tun+ -j ACCEPT
+    iptables -A FORWARD -i tun+ -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+    iptables -A FORWARD -i eth0 -o tun+ -m state --state RELATED,ESTABLISHED -j ACCEPT
+    iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+    iptables -A OUTPUT -o tun+ -j ACCEPT
+    "
+
+}
+
+openvpn_start_vpn () {
+  docker container exec ${PREFIX}_setupc /bin/sh -c "
+    openvpn  --dev tun0 --daemon
+    sleep 1
+    ifconfig tun0 up"
+}
+
+openvpn_setup_client () {
+
+  local MY_IP=$(dig +short ${DOMAIN})
+
+  docker container exec ${PREFIX}_setupc /bin/sh -c "/usr/share/easy-rsa/easyrsa --batch build-client-full  ${1}  nopass"
+
+  echo "client"
+  echo "dev tun"
+  echo "proto udp"
+  echo "remote ${MY_IP} 1194"
+  echo "resolv-retry infinite"
+  echo "nobind"
+  echo "persist-key"
+  echo "persist-tun"
+  echo "remote-cert-tls server"
+  echo "auth SHA512"
+  echo "cipher AES-256-CBC"
+  echo "ignore-unknown-option block-outside-dns"
+  echo "block-outside-dns"
+  echo "verb 3"
+  echo "<dh>"
+  docker container exec ${PREFIX}_setupc /bin/sh -c "cat /pki/dh.pem"
+  echo "</dh>"
+  echo "<ca>"
+  docker container exec ${PREFIX}_setupc /bin/sh -c "cat /pki/ca.crt"
+  echo "</ca>"
+  echo "<cert>"
+  docker container exec ${PREFIX}_setupc /bin/sh -c "cat /pki/issued/${1}.crt"
+  echo "</cert>"
+  echo "<key>"
+  docker container exec ${PREFIX}_setupc /bin/sh -c "cat /pki/private/${1}.key"
+  echo "</key>"
+}
+
+openvpn_setup_vpn
+openvpn_setup_client ${MASTER_USER}
+openvpn_setup_client ${DEMO_USER}
+openvpn_start_vpn
