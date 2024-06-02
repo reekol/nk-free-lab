951ea3cea1692c685a21729430bafae326690f8d
reekol <cupuyc@gmail.com>
2024-06-02 12:58:09 +0300

    Update RangeExtender_NAPT_WebServer_OLED_SSD1306.ino

--- ESP8266/RangeExtender_NAPT_WebServer_OLED_SSD1306.ino
+++ ESP8266/RangeExtender_NAPT_WebServer_OLED_SSD1306.ino
@@ -1,3 +1,5 @@
+// WIP https request
+
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <lwip/napt.h>
@@ -7,11 +9,11 @@
 #include <U8g2lib.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
-#include <WiFiClientSecureBearSSL.h>
+//#include <WiFiClientSecureBearSSL.h>
+#include <WiFiClientSecure.h>
 
 #define STAPSKEXT "76543210"
 #define EXTNAME "ESP_GUEST"
-#define REQUEST_TO "https://www.howsmyssl.com/a/check"
 #define EMPTY_LINE "                    "
 #define NAPT 1000
 #define NAPT_PORT 10
@@ -20,6 +22,7 @@ ESP8266WiFiMulti wifiMulti;
 ESP8266WebServer http_server(80);
 
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0,/*clock=*/14,/*data*/12, U8X8_PIN_NONE);
+const char*  server = "www.howsmyssl.com";  // Server URL
 
 void handleRoot() {
   http_server.send(200, "text/html", "<h1>You are connected</h1>");
@@ -38,35 +41,45 @@ void wifiReconnect(){
   server.setDns(WiFi.dnsIP(0));
 }
 
+const char* rootCACertificate = "";
+
 void httpsSendData(){
 
-  std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+  WiFiClientSecure *client = new WiFiClientSecure;
+  if(client) {
+    // set secure client with certificate
+//    client->setCACert(rootCACertificate);
+    client->setInsecure();
+    //create an HTTPClient instance
+    HTTPClient https;
 
-  //  client->setFingerprint(fingerprint_sni_cloudflaressl_com);
-  // Or, if you happy to ignore the SSL certificate, then use the following line instead:
-     client->setInsecure();
-
-  HTTPClient https;
-
-  if (https.begin(*client, REQUEST_TO)) {  // HTTPS
-      Serial.print("[HTTPS] 1) GET...\n");
+    //Initializing an HTTPS communication using the secure client
+    Serial.print("[HTTPS] begin...\n");
+    if (https.begin(*client, "https://www.howsmyssl.com/a/check")) {  // HTTPS
+      Serial.print("[HTTPS] GET...\n");
       // start connection and send HTTP header
       int httpCode = https.GET();
       // httpCode will be negative on error
       if (httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTPS] 2) GET... code: %d\n", httpCode);
-        // file found at server
+      // HTTP header has been send and Server response header has been handled
+       Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+      // file found at server
         if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          // print server response payload
           String payload = https.getString();
           Serial.println(payload);
         }
-      } else {
-        Serial.printf("[HTTPS] 3) GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      }
+      else {
+        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
       }
       https.end();
     }
   }
+  else {
+    Serial.printf("[HTTPS] Unable to connect\n");
+  }
+}
   
 void setup() {
   Serial.begin(115200, SERIAL_8N1);
@@ -133,8 +146,9 @@ void loop() {
       u8g2.drawStr(5,45, EMPTY_LINE);
       u8g2.drawStr(5,45, WiFi.softAPIP().toString().c_str());
       u8g2.sendBuffer();
-
       delay(3000);
       httpsSendData();
     }
+
+    delay(1000);
 }
